12b8a2e781bdfbd5fea47afc7607fda0d7b03947
Tizian Lamatsch <tlamatsch@godaddy.com>
2022-02-08 11:57:46 +0100

    check if files already exist and verify their md5

--- installer.sh
+++ installer.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+ALLREDIST_FILE = "./allredist.tar.xz"
+ALLREDIST_MD5 = "8bfab2e4a4682d9bcf79926544053b76"
+PHOTOSHOP_FILE = "./AdobePhotoshop2021.tar.xz"
+PHOTOSHOP_MD5 = "cccb6715180b86e1eb8c1d7bd4a8a4e8"
+
 echo "Welcome to Photoshop installer"
 echo "Would you like to install Camera Raw with photoshop ? (This will prompt the camera raw installer at the end)"
 echo "1 = yes 0 = no"
@@ -11,10 +16,25 @@ curl -L https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winet
 chmod +x winetricks
 
 WINEPREFIX=~/.WineApps/Adobe-Photoshop wineboot
-
 WINEPREFIX=~/.WineApps/Adobe-Photoshop ./winetricks win10
 
-curl -L "https://drive.google.com/uc?export=download&id=1qcmyHzWerZ39OhW0y4VQ-hOy7639bJPO" > allredist.tar.xz
+
+if [ ! -f "$ALLREDIST_FILE" ]; then
+	curl -L "https://drive.google.com/uc?export=download&id=1qcmyHzWerZ39OhW0y4VQ-hOy7639bJPO" > allredist.tar.xz
+fi
+
+if [ ! -f "$PHOTOSHOP_FILE" ]; then
+	curl -L "https://drive.google.com/uc?export=download&id=1qcmyHzWerZ39OhW0y4VQ-hOy7639bJPO" > allredist.tar.xz
+fi
+
+if ! md5sum --status -c <(echo $ALLREDIST_MD5 $ALLREDIST_FILE); then
+	echo "ERROR: md5sum of $ALLREDIST_FILE did not match! Please download the file again."
+fi
+
+if ! md5sum --status -c <(echo $PHOTOSHOP_MD5 $PHOTOSHOP_FILE); then
+	echo "ERROR: md5sum of $PHOTOSHOP_FILE did not match! Please download the file again."
+fi
+
 tar -xf allredist.tar.xz
 rm -rf allredist.tar.xz
 curl -L "https://download854.mediafire.com/kj7h8gkorsvg/dhvztovo7gj738e/AdobePhotoshop2021.tar.xz" > AdobePhotoshop2021.tar.xz
